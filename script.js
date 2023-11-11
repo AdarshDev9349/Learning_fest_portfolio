85a07b027366cfb59f85d8b000952901f0618097
AdarshDev9349 <adarshdev9349@gmail.com>
2023-11-11 21:42:34 +0530

    commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var typed = new Typed('#typed-text', {
+    strings: ["I am a web developer","also interested in AI ML"],
+    typeSpeed: 30,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false
+});
+
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+
+  gsap.from(".about", { duration: 6, opacity: 0, y: -50, ease: "power4.out" });
+
+
+
+  
